68f753906b21772cca2b482d975cb4dcb2bbd379
umbynos <umberto.baldi@edu.unito.it>
2019-07-05 09:19:39 +0200

    add methods to initialize bins (starting point for the valid solution)

--- colored_bin_packing.py
+++ colored_bin_packing.py
@@ -1,9 +1,12 @@
 import sys
+bins_weight = 150
 
 
 def main():
     instance_file = sys.argv[1]
     item_list = infer_file(instance_file)
+    bins = assign_bin_1(item_list)
+    bins = assign_bin_2(item_list)
 
 
 def infer_file(instance_file):  # read input file and return a list of items
@@ -15,6 +18,34 @@ def infer_file(instance_file):  # read input file and return a list of items
         item_list.append([item+1, weight, color])
     input_file.close()
     return item_list
+
+
+# assign items to bins in a dumb way (one item for each bin)
+def assign_bin_1(item_list):
+    bins = []  # bins are being represented as a list
+    for item in item_list:
+        if item[1] <= bins_weight:
+            bins.append([item])  # add one bin
+    return bins
+
+
+# assign items to bins in a smarter way (fills a bin, before going to next one)
+def assign_bin_2(item_list):
+    bins = [[]]  # bins are being represented as a list
+    i = 0
+    bin_weight = bins_weight
+    for item in item_list:
+        if item[1] <= bins_weight:
+            if item[1] <= bin_weight:
+                bins[i].append(item)
+                bin_weight -= item[1]
+            else:
+                i += 1  # go to the next bin
+                bin_weight = bins_weight
+                bins.append([])
+                bins[i].append(item)
+                bin_weight -= item[1]
+    return bins
 
 
 if __name__ == '__main__':
